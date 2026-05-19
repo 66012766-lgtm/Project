c7baccca7877fe25f887358f34103d1262c0b8bc
66012766-lgtm <66012766@rbac.ac.th>
2026-05-19 13:55:45 +0700

    update upload system

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(
   })
 );
 
-app.options("*", cors());
+app.options(/.*/, cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
